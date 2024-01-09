7055548e7bf945d4eb95241f874c17bd88b76bd5
Billy <billy.xu99@gmail.com>
2024-01-10 06:55:38 +1000

    Load current 20 pokemon from api

--- poke-api-project/src/App.jsx
+++ poke-api-project/src/App.jsx
@@ -6,7 +6,22 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 
+
 function App() {
+
+  const [pokeData, setPokeData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+
+  const pokeFun=async () => {
+    setLoading(true)
+    const res=await axios.get(url)
+    console.log(res.data.results)
+  }
+
+  useEffect(() => {
+    pokeFun() 
+  }, [url])
 
   return (
     <>
