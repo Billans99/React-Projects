21e76ec83b46cd447068a77b3caf53ee2a264445
Billy <billy.xu99@gmail.com>
2024-01-10 15:58:02 +1000

    Changed names of each pokemon using the api

--- poke-api-project/src/App.jsx
+++ poke-api-project/src/App.jsx
@@ -12,11 +12,29 @@ function App() {
   const [pokeData, setPokeData] = useState([])
   const [loading, setLoading] = useState(true)
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
+  const [nextUrl, setNextUrl] = useState()
+  const [prevUrl, setPrevUrl] = useState()
 
   const pokeFun=async () => {
     setLoading(true)
     const res=await axios.get(url)
-    console.log(res.data.results)
+    // console.log(res.data.results)
+    setNextUrl(res.data.next)
+    setPrevUrl(res.data.previous)
+    getPokemon(res.data.results)
+    setLoading(false) 
+    // console.log(pokeData)
+  }
+  const getPokemon=async(res) => {
+    res.map(async(item) => {
+      const result=await axios.get(item.url)
+      // console.log(result.data)
+      setPokeData(state => {
+        state=[...state, result.data]
+        return state
+      })
+    })
+
   }
 
   useEffect(() => {
@@ -26,26 +44,7 @@ function App() {
   return (
     <>
       <Header/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      <Card pokemon={pokeData} loading={loading}/>
       <Load/>
     </>
   )
--- poke-api-project/src/Card.jsx
+++ poke-api-project/src/Card.jsx
@@ -3,18 +3,31 @@ import React from 'react'
  /*import pokeAvatar from 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png' */
 
 // Pokemon profile card: includes container, image, and Pokemon info
-function Card() {
+
+function Card({ pokemon, loading }) { 
+    console.log(pokemon)
+
     return(
     <> 
-    <div className="card-container"> 
-        <img className="pokeImage" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="Pokemon character image">
-        </img>
-        <div className="info-container">
-            <h3 className="name">Pikachu</h3>
-            {/* This is will differ from abilties, we are using type */}
-            <p className="type-info">Electric</p>
-        </div>
-    </div> 
+    {
+        loading ? <h1>Loading...</h1> : 
+        pokemon.map((item) => {
+            return(
+                <>
+                    <div className="card-container"> 
+                        <img className="pokeImage" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="Pokemon character image">
+                        </img>
+                        <div className="info-container">
+                            <h3 className="name">{item.name}</h3>
+                            {/* This is will differ from abilties, we are using type */}
+                            <p className="type-info">Electric</p>
+                        </div>
+                    </div> 
+                </>
+            )
+        })
+    }
+  
     </>
     )
 }
