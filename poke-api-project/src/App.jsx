1d83c594df444c7be593d96675b0099838634b4a
Billy <billy.xu99@gmail.com>
2024-01-11 11:03:40 +1000

    Fixed issue with loading extra copy of each Pokemon

--- poke-api-project/src/App.jsx
+++ poke-api-project/src/App.jsx
@@ -5,46 +5,41 @@ import axios from 'axios'
 
 import { useEffect, useState } from 'react'
 
-
-
 function App() {
 
   const [pokeData, setPokeData] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/")
   const [nextUrl, setNextUrl] = useState()
   const [prevUrl, setPrevUrl] = useState()
 
   const pokeFun=async () => {
-    console.log("preloading", loading)
+    if (loading) return
+
     setLoading(true)
     console.log("postloading", loading)
     const res=await axios.get(url)
     console.log(res)
     setNextUrl(res.data.next)
     setPrevUrl(res.data.previous)
-    getPokemon(res.data.results)
-    setLoading(false) 
+    await getPokemon(res.data.results)
+    setLoading(false)
     // console.log(pokeData)
   }
   const getPokemon=async(res) => {
-    res.forEach(async(item) => {
-      const result=await axios.get(item.url)
-      console.log("result", result)
-      setPokeData(state => {
-        state=[...state, result.data]
-        state.sort((a, b) => a.id > b.id ? 1 : -1)
-        return state
+    const responses = await Promise.all(
+      res.map((pokemon) => {
+        return axios.get(pokemon.url)
       })
-    })
-    
+    )
+
+    const newPokeData = responses.map((pokemon) => pokemon.data)
+    setPokeData(newPokeData)
   }
-  console.log("pokeData", pokeData)
 
   useEffect(() => {
-    setPokeData([])
-    pokeFun() 
-  }, [url])
+    pokeFun()
+  }, [])
 
   return (
     <>
